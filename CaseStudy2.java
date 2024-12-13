6a72b9244422553864ef927bfba0213c2e479a07
fael83 <rafaellokajaya@gmail.com>
2024-12-13 15:08:59 +0700

    Function addOrder

--- CaseStudy2.java
+++ CaseStudy2.java
@@ -27,4 +27,47 @@ public class CaseStudy2 {
             } 
         }
     }
+    
+    static void addOrder(){
+        System.out.print("Insert customer name: ");
+        overall [numData][0] = sc.next();
+        System.out.print("Insert table number: ");
+        overall [numData][1] = sc.next();
+        displayMenus(); 
+        int sum ;
+        int total = 0;
+        int num=2;
+        int hasilAkhir =0;
+        while (true) {
+            System.out.print("\nSelect a menu (enter the menu number, or 0 to finish): ");
+            int coffee = sc.nextInt();
+            if (coffee != 0 && coffee <= 4) {
+                while (true) {
+                    System.out.print("Enter the number of item for " + menu[coffee - 1] + ": ");
+                    total = sc.nextInt();
+                    sum = 0;
+                    if (total <= 0) {
+                        System.out.println("Enter amount greater than 0");
+                    } else {
+                        sum += price[coffee-1] * total;
+                        String summ = Integer.toString(sum);
+                        hasilAkhir += sum;
+                        overall[numData][num] = menu[coffee-1] + " x " + total + " = " + summ;
+                        num++;
+                        break;
+                    }
+                }
+            }else if(coffee >= 4){
+                System.out.println("G ada boss");
+            }else { 
+                String totalAhir = Integer.toString(hasilAkhir);
+                overall[numData][8] = totalAhir;
+                break;
+            }
+        }
+        System.out.println("\nOrder successfully added.");
+        System.out.println("Total order price: " + hasilAkhir);
+        System.out.println();
+        numData += 1;
+    }
 }
