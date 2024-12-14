12c3f5aeba7c0e8aed3c006e9d61e49bcf4e94a4
KarW010 <kartikostudy@gmail.com>
2024-12-14 20:36:04 +0700

    Full Program Code

--- CaseStudy2.java
+++ CaseStudy2.java
@@ -1,14 +1,14 @@
 import java.util.Scanner;
 
 public class CaseStudy2 {
-    static String[] menu = {"Kopi Hitam", "Latte", "Teh Tarik", "Mie Goreng"};
+    static String[] menu = {"Black Coffee", "Latte", "Tarik Tea", "Fried Noodle"};
     static int[] price = {15000, 22000, 12000, 18000};
-    static String [][] overall = new String [100][6]; 
+    static String [][] overall = new String [100][9]; 
     static int numData = 0;
     static Scanner sc = new Scanner(System.in);
     public static void main(String[] args) {
         while (true) {
-            System.out.println("===== MENU UTAMA =====");
+            System.out.println("===== MAIN MENU =====");
             System.out.println("1. Add Order");
             System.out.println("2. Show order list");
             System.out.println("3. Exit");
@@ -37,36 +37,37 @@ public class CaseStudy2 {
         int sum ;
         int total = 0;
         int num=2;
-        int hasilAkhir =0;
+        int finalResult =0;
         while (true) {
             System.out.print("\nSelect a menu (enter the menu number, or 0 to finish): ");
-            int coffee = sc.nextInt();
-            if (coffee != 0 && coffee <= 4) {
+            int menuNumber = sc.nextInt();
+            if (menuNumber != 0 && menuNumber <= 4 && menuNumber >= 0) {
                 while (true) {
-                    System.out.print("Enter the number of item for " + menu[coffee - 1] + ": ");
+                    System.out.print("Enter the number of item for " + menu[menuNumber - 1] + ": ");
                     total = sc.nextInt();
                     sum = 0;
                     if (total <= 0) {
                         System.out.println("Enter amount greater than 0");
                     } else {
-                        sum += price[coffee-1] * total;
+                        sum += price[menuNumber-1] * total;
                         String summ = Integer.toString(sum);
-                        hasilAkhir += sum;
-                        overall[numData][num] = menu[coffee-1] + " x " + total + " = " + summ;
+                        finalResult += sum;
+                        overall[numData][num] = menu[menuNumber-1] + " x " + total + " = Rp " + summ;
                         num++;
                         break;
                     }
                 }
-            }else if(coffee >= 4){
-                System.out.println("G ada boss");
+            }else if(menuNumber > 4 || menuNumber < 0){
+                System.out.println("The menu is not available");
+                continue;
             }else { 
-                String totalAhir = Integer.toString(hasilAkhir);
-                overall[numData][8] = totalAhir;
+                String finalTotal = Integer.toString(finalResult);
+                overall[numData][8] = finalTotal;
                 break;
             }
         }
         System.out.println("\nOrder successfully added.");
-        System.out.println("Total order price: " + hasilAkhir);
+        System.out.println("Total order price: " + finalResult);
         System.out.println();
         numData += 1;
     }
@@ -77,4 +78,31 @@ public class CaseStudy2 {
         System.out.println("3. "+ menu[2] +" - Rp " +  price[2]);
         System.out.println("4. " + menu [3] +" - Rp " +  price[3]);
     }
+    static void displayOverall(){
+        if (numData == 0) {
+            System.out.println("\nThere is no order.");
+        } else {
+            System.out.println("\n==== ORDER LISTS ====");
+            for (int i = 0; i < numData; i++){
+                System.out.println("Customer Name: " + overall[i][0]);
+                System.out.println("Table Number: " + overall[i][1]);
+                System.out.println("Order details:");
+                while (true) {
+                for (int j = 2; j < overall[i].length; j++){
+                    if (overall[i][j] == null) {
+                        continue;
+                    } else {
+                        if(j > 7) break;
+                        System.out.println("- " + overall[i][j]);
+                    }
+                    
+                    }
+                    System.out.println("Total Order Price: " + overall[i][8]);
+                    System.out.println("----------------------------");
+                    break;
+                }
+            }
+            System.out.println();
+        }
+}
 }
